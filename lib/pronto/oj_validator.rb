5f149f2a795a8250cbd9552991bfacb23a1fa9cc
Mark Lee <pronto+oj@lazymalevolence.com>
2015-03-23 22:07:47 -0700

    Successfully tested some validation errors

--- lib/pronto/oj_validator.rb
+++ lib/pronto/oj_validator.rb
@@ -1,3 +1,4 @@
+require 'oj'
 require 'pronto'
 
 module Pronto
@@ -12,13 +13,15 @@ module Pronto
 
     def inspect(patch)
       begin
-        Oj.load_file(patch.new_file_full_path)
+        Oj.load_file(patch.new_file_full_path.to_s)
       rescue Oj::ParseError => e
         match = /^(.* at) line (\d+), (column \d+)/.match(e.message)
         raise unless match
-        line_number = match[2]
-        line = patch.added_lines.detect { |added_line| added_line.new_line_no == line_number }
-        return [new_message("#{match[1]} #{match[3]}", line)] if line
+        msg_prefix = match[1]
+        line_numbers = [match[2].to_i]
+        line_numbers << line_numbers[0] - 1 if msg_prefix.end_with?('not terminated at')
+        line = patch.added_lines.detect { |added_line| line_numbers.include?(added_line.new_lineno) }
+        return [new_message("#{msg_prefix} #{match[3]}", line)] if line
       end
       []
     end
@@ -27,7 +30,7 @@ module Pronto
       path = line.patch.delta.new_file[:path]
       level = :error
 
-      Message.new(path, line, level, offence['reason'])
+      Message.new(path, line, level, offence)
     end
 
     def json_file?(path)
