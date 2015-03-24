fb9010881e61b7f085a1eb52c8cd7d2494b82576
Mark Lee <pronto+oj@lazymalevolence.com>
2015-03-23 22:39:21 -0700

    Refactor for clarity

--- lib/pronto/oj_validator.rb
+++ lib/pronto/oj_validator.rb
@@ -3,6 +3,8 @@ require 'pronto'
 
 module Pronto
   class OjValidator < Runner
+    MSG_REGEX = /^(?<msg_prefix>.* at) line (?<line_number>\d+), (?<msg_suffix>column \d+)/
+
     def run(patches, _)
       return [] unless patches
 
@@ -12,18 +14,28 @@ module Pronto
     end
 
     def inspect(patch)
+      messages = []
+
       begin
         Oj.load_file(patch.new_file_full_path.to_s)
       rescue Oj::ParseError => e
-        match = /^(.* at) line (\d+), (column \d+)/.match(e.message)
-        raise unless match
-        msg_prefix = match[1]
-        line_numbers = [match[2].to_i]
-        line_numbers << line_numbers[0] - 1 if msg_prefix.end_with?('not terminated at')
-        line = patch.added_lines.detect { |added_line| line_numbers.include?(added_line.new_lineno) }
-        return [new_message("#{msg_prefix} #{match[3]}", line)] if line
+        @match = MSG_REGEX.match(e.message)
+        raise unless @match
+
+        line = patch.added_lines.detect { |added| line_numbers.include?(added.new_lineno) }
+        messages << new_message("#{@match[:msg_prefix]} #{@match[:msg_suffix]}", line) if line
       end
-      []
+
+      messages
+    end
+
+    def line_numbers
+      unless @line_numbers
+        @line_numbers = [@match[:line_number].to_i]
+        @line_numbers << @line_numbers[0] - 1 if @match[:msg_prefix].end_with?('not terminated at')
+      end
+
+      @line_numbers
     end
 
     def new_message(offense, line)
